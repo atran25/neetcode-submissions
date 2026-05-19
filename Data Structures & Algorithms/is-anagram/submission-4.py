41d60f4aa923eafffc015925b525b9507722e232
Anthony Tran <atran2555@gmail.com>
2026-05-18 17:18:21 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        letter_count = defaultdict(int)
+        for char in s:
+            letter_count[char] += 1
+
+        for char in t:
+            letter_count[char] -= 1
+
+        for count in letter_count.values():
+            if count != 0:
+                return False
+        return True
